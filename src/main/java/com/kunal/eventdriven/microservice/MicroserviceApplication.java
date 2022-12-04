b96a9f0d51a9cf4c92222dec3b8667103c748450
Kumar Kunal <kkunal2022@gmail.com>
2022-12-04 22:17:02 +0300

    Logger Addition

--- src/main/java/com/kunal/eventdriven/microservice/MicroserviceApplication.java
+++ src/main/java/com/kunal/eventdriven/microservice/MicroserviceApplication.java
@@ -1,13 +1,17 @@
 package com.kunal.eventdriven.microservice;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MicroserviceApplication {
+	private static final Logger logger = LogManager.getLogger(MicroserviceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceApplication.class, args);
+		logger.info("Spring Boot Event Driven Microservice Application Started Successfully ");
 	}
 
 }
